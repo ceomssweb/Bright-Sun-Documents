184a61c3c3f0adce82472e09b6ccc92f46ee8afa
ceomssweb <84248225+ceomssweb@users.noreply.github.com>
2022-08-20 17:49:12 +0530

    function was removed for time being

--- bright-sun-documents/src/app/app.module.ts
+++ bright-sun-documents/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { initializeApp } from "firebase/app";
+import { getFunctions  } from "firebase/functions";
 import { getAnalytics } from "firebase/analytics";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
@@ -36,6 +37,7 @@ export const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const storage = getStorage(app);
+const functions = getFunctions(app);
 @NgModule({
   declarations: [
     AppComponent
